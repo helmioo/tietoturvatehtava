81226db8aadfeaa5b4fe1a68bc0d8a40637a0463
helmioo <81010386+helmioo@users.noreply.github.com>
2021-12-18 17:06:22 +0200

    viimeiset kommentit

--- inc/headers.php
+++ inc/headers.php
@@ -1,4 +1,5 @@
 <?php
+// Sisällytetään Firebase-kirjasto
 include('fb/BeforeValidException.php');
 include('fb/ExpiredException.php');
 include('fb/JWK.php');
@@ -6,17 +7,15 @@ include('fb/JWT.php');
 include('fb/Key.php');
 include('fb/SignatureInvalidException.php');
 
-// Allow from any origin
+// Sallitaan liikenne paikalliselta serveriltä
 if (isset($_SERVER['HTTP_ORIGIN'])) {
-    // Decide if the origin in $_SERVER['HTTP_ORIGIN'] is one
-    // you want to allow, and if so:
     header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
     header('Access-Control-Allow-Credentials: true');
     header('Content-Type: application/json');
     header('Access-Control-Max-Age: 3600');
 }
 
-// Access-Control headerit käyttöön kun niitä tarvitaan
+// Access-Control-Headerit käyttöön kun niitä tarvitaan
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
     
     if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
--- register.php
+++ register.php
@@ -3,4 +3,5 @@
 require_once './inc/functions.php';
 require_once './inc/headers.php';
 
+// Kutsutaan käyttäjän lisäys-funktiota
 createUser(openDb());
